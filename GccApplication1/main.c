da5a57843fc5bde6c60109f4dab121a0bce78e43
Adam Klima <adamklimanededza@gmail.com>
2019-03-22 11:20:04 +0100

    Initial Commit

---
+++ GccApplication1/main.c
@@ -0,0 +1,44 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 22/03/2019 08:36:57
+ * Author : Adam Klima
+ */ 
+
+#include <stdio.h>
+#include <stdio_driver.h>
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+
+
+
+
+int main(void)
+{
+    stdioCreate(0);
+	sei();
+	
+	puts("Program Started!");
+	
+	uint16_t counter = 0;
+	
+	
+    while (1) 
+    {
+		printf(" The counter value: %05d and in hex> %04X\n", counter, counter);
+		counter++;
+		
+		if(stdioInputWaiting())
+		{
+			printf("###>%c\n", getchar());
+			
+		}
+		
+		_delay_ms(500);
+
+    }
+}
+
